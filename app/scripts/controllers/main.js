5231f5de199ff03586bbe38f9bd0f93fcdb5fe1f
ericterpstra <eterps@gmail.com>
2012-10-17 16:19:55 -0500

    Added lots o' comments. Started StackMob service.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
+/**
+ * This is the main controller for the application.  There is only one right now,
+ * given the simplistic nature of the application.  
+ *
+ */
+var MainCtrl = ngSignItApp.controller('MainCtrl', function($scope,DataService) {
 
-var MainCtrl = ngSignItApp.controller('MainCtrl', function($scope,ParseService) {
+  /**
+   *  Controller Functions
+   */
 
-  // Controller Functions...
-
+  // Clear the form fields and scope data
   $scope.clearData = function() {
     $scope.user = {
       firstName: null,
@@ -14,47 +21,66 @@ var MainCtrl = ngSignItApp.controller('MainCtrl', function($scope,ParseService) 
     };
   };
 
-  $scope.saveSignature = function saveSignature() {  
-    if ( $scope.sigForm.$valid ) {
-      $scope.user.petitionId = $scope.select2;
-      ParseService.saveSignature($scope.user, function() {
-        $scope.clearData();
-        $scope.getSignatures($scope.select2);
-      });  
-    }
-  };
-
+  // Change handler for the select2 list of petitions
   $scope.pickPetition = function pickPetition(id) {  
+    // Check to see if a valid petition is selected
     if(id) {
+      // Show the description of the selected petition and enable the form
+      // Use the 'get' method from backbone.js collection to get the proper petition, 
+      // then use the model's 'get' method to get the Description field data. 
       $scope.petitionDescription = $scope.petitionCollection.get(id).get('Description');
       $scope.formDisabled = false;
     } else {
+      // If the default selection in the select2 control is chosen, then reset everything.
       $scope.petitionDescription = "Choose a cause / petition / whatever in the box above, then plop down your name, email address, and John Hancock below to show your support."
       $scope.formDisabled = true;
       $scope.clearData();
     }
   };
 
+  // Called on startup, and whenever the list needs refreshing.
   $scope.getSignatures = function getSignatures (petitionId) {
     if (petitionId.length > 0){
-      ParseService.getSignatures(petitionId,function (results) {
+      // Call the service and fetch the list of signatures that match the given petition ID
+      DataService.getSignatures(petitionId,function (results) {
         $scope.$apply(function() {
+          // Apply the results to the signatureList model so it will refresh the table in the view
           $scope.signatureList = results;
         });
       });
     } else {
+      // Clear the list if an invalid petition is chosen
       $scope.signatureList = [];
     }
   };
 
+  // Click handler for the Save button. Saves the form to the back-end service.
+  $scope.saveSignature = function saveSignature() {  
+    // Check for validity. AngularJS will set $valid == true if all form requirements are met.
+    if ( $scope.sigForm.$valid ) {
+      // Save the selected petition ID along with the form data.
+      $scope.user.petitionId = $scope.select2;
+      // Call the saveSignature method on the service module
+      DataService.saveSignature($scope.user, function() {
+        // When the service call is finished, clear the form and reload the signature list.
+        $scope.clearData();
+        $scope.getSignatures($scope.select2);
+      });  
+    }
+  };
 
-  // Do Stuff...
 
+  /**
+   * On Startup...
+   */
+
+  // Set defaults and clear the form
   $scope.signatureList = [];
   $scope.clearData();
   $scope.formDisabled = true;
 
-  ParseService.getPetitions(function (results) {
+  // Fetch the list of petitions from the back-end service
+  DataService.getPetitions(function (results) {
     $scope.$apply(function() {
       $scope.petitionCollection = results;
       $scope.petitions = results.models;
--- app/scripts/modules/services.js
+++ app/scripts/modules/services.js
@@ -1,29 +1,42 @@
 /**
- * parseService Module
+ * DataService Module
  *
- *  Wrapper for Parse.com JavaScript API
+ *  Service Provider for Back-end APIs
  */
 angular.module('DataServices', []) 
+/**
+ * Parse Service
+ * Use Parse.com as a back-end for the application.
+ */
 .factory('ParseService', function(){
-    // Initialize Parse API and objects.
+    // Initialize Parse API and objects. Please don't use this key in your own apps. It won't work anyway.
     Parse.initialize("d46qFYur20F0T5XqD61wGWHtey9z3Q1ouBkm2t3X", "8Mo9iTOBJmmcPwAYcT4EppSBZlQulHn75RUT9Sny");
 
+    // Define Parse Model and Collection for Signature records (firstName, lastName, email, signature, and petitionId)
     var Signature = Parse.Object.extend("Signature");
     var SignatureCollection = Parse.Collection.extend({ model: Signature });
 
+    // Define Parse Model and Collection for Petitions. 
     var Petition = Parse.Object.extend("Petition");
     var PetitionCollection = Parse.Collection.extend({ model: Petition });
 
     /**
-     * Service Object
+     * ParseService Object
+     * This is what is used by the main controller to save and retrieve data from Parse.com.
+     * Moving all the Parse.com specific stuff into a service allows me to later swap it out 
+     * with another back-end service provider without modifying my controller much, if at all.
      */
     var ParseService = {
 
+      // Retrieve all petitions
       getPetitions : function getPetitions(callback) {
+        // Instantiate a petition collection
         var petitions = new PetitionCollection();
 
+        // Use Parse's fetch method (a modified version of backbone.js fetch) to get all the petitions.
         petitions.fetch({
           success: function (results) {
+              // Send the petition collection back to the caller if it is succesfully populated. 
               callback(petitions);
           },
           error: function ( results,error) {
@@ -32,6 +45,7 @@ angular.module('DataServices', [])
         });
       },
 
+      // Save the data from the signature form to Parse.com
       saveSignature : function saveSignature(data, callback){
         var sig = new Signature();
         sig.save( data,
@@ -46,10 +60,12 @@ angular.module('DataServices', [])
         );
       },
 
+      // Get signature data for a specified petition
       getSignatures : function getSignatures(petitionId, callback) {
-        var signatures = new SignatureCollection();
+        // Create a new Parse Query object in order to search Signature records by petitionId
         var query = new Parse.Query(Signature);
         query.equalTo("petitionId", petitionId);
+        // Use the find method to retreive all signatures with the given petitionId
         query.find({
           success: function (results) {
             callback(results);
@@ -62,11 +78,17 @@ angular.module('DataServices', [])
     
     };
 
-    // The factory function returns CatsService, which is injected into controllers.
+    // The factory function returns ParseService, which is injected into controllers.
     return ParseService;
 })
+/**
+ * StackMob Service
+ * This is a service to use StackMob as a back-end for the application.
+ */
 .factory('StackMobService', function(){
-    // COMING SOON!
+    // COMING SOON! Use StacMob instead of Parse.com as a back-end provider.
+    // STackmob also bases their javascript API on backbone.js, so most of the data is saved and retrieved 
+    // in a manner similar to Parse.com.  It should be fairly trivial for a controller to use one service or the other
     
     /**
      * Service Object
@@ -81,6 +103,15 @@ angular.module('DataServices', [])
     
     };
 
-    // The factory function returns CatsService, which is injected into controllers.
+    // The factory function returns StackMobService, which is injected into controllers.
     return StackMobService;
-});;
+})
+/**
+ * DataService is a simple adapter that returns either the Parse Service or StackMob Service.
+ * This service is injected into the Main Controller
+ */
+.factory('DataService', function (ParseService,StackMobService) {
+  // Eventually some logic will go here to choose between StackMob and Parse as a backend.
+  // Right now we are just using Parse, so ParseService is used as the main DataService
+  return ParseService;
+});
